ce26ff3a15df42cf35edb01c3fb5b1890e72ed43
abhijeeeth <112998926+abhijeeeth@users.noreply.github.com>
2023-11-28 11:49:20 +0530

    Pushing for review again

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -12,7 +12,7 @@ const AboutUs = () => {
     <p className="text-gray-700 leading-relaxed text-center">We adhere to strict data protection protocols and employ industry-best practices to safeguard your information. We never share or sell client data with third parties without explicit consent, and we ensure that access to your data is restricted to authorized personnel.</p>
      
      <h3 className="text-gray-600 font-bold text-2xl mb-4 text-center mt-8">Your Projects Remain Yours</h3>
-    <p className="text-gray-700 leading-relaxed text-center">We respect the ownership and originality of our clients' creations. We do not display client projects on our website without their express permission. Your projects are your pride, and we honor your right to control their showcase.</p>
+    <p className="text-gray-700 leading-relaxed text-center">We respect the ownership and originality of our clients creations. We do not display client projects on our website without their express permission. Your projects are your pride, and we honor your right to control their showcase.</p>
      
   </div>
   );
