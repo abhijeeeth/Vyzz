7ab72743079e9d93a63b74a2057745f8d0d53c6a
abhijeeeth <112998926+abhijeeeth@users.noreply.github.com>
2023-11-28 11:43:21 +0530

    added  about

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Link from 'next/link';
 
 const AboutUs = () => {
   return (
     <div className="p-16 bg-gradient-to-b from-gray-300/50  via-white to-gray-300  shadow-lg mx-16 mt-8 font-">
-    <h3 className="text-gray-700 font-bold text-3xl mb-4 text-center mx-16">Why Visonix ?</h3>
+      
+    <h3 className="text-gray-700 font-bold text-3xl mb-4 text-center mx-16">
+      Why <Link href='/'>Visonix</Link>  ?
+      
+    </h3>
     <h3 className="text-gray-600 font-bold text-2xl mb-4 text-center">We develop websites with client privacy at the forefront.</h3>
     <p className="text-gray-700 leading-relaxed text-center">We adhere to strict data protection protocols and employ industry-best practices to safeguard your information. We never share or sell client data with third parties without explicit consent, and we ensure that access to your data is restricted to authorized personnel.</p>
      
